717bb28a6c57b83fee7f34b37ac49f85a6121aa2
AndrewHamal <73131641+AndrewHamal@users.noreply.github.com>
2024-04-15 14:44:09 +0545

    fixed

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     const [isMobile] = useMediaQuery(`(max-width: 808px)`);
     return (
         <Box className={`border-t ${isMobile ? 'pt-2' : 'pt-11'}`}>
-            <img src="/assets/landscape.jpg" width={"100%"} className='cover absolute bottom-0 z-[-1] opacity-[0.2] left-0 right-0' alt="" />
+            <img src="/assets/landscape.jpg" width={"100%"} className='landscape-footer cover absolute bottom-0 z-[-1] opacity-[0.2] left-0 right-0' alt="" />
             <FooterOptions />
         </Box>
     )
